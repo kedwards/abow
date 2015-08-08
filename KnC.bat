48518c8cdacde7c8756262e97e31ba59e6edc508
Kevin Edwards <kedwards@kncedwards.com>
2015-08-08 15:10:36 -0700

    small tweaks

--- KnC.bat
+++ KnC.bat
@@ -81,17 +81,24 @@ exit /b 1
 exit /b 0
 
 :GET_SETUP
-	echo %prefix% Retrieving setup..
-	:: Download latest setup-%arch%.exe
-	if not exist %rootdir% mkdir %rootdir%
-    if not exist %rootdir%\cygwin.exe echo %prefix% ERROR: %rootdir%\cygwin.exe cannot be found. && exit /b 1
+	if "%autoupdate%"=="0" exit /b 0
 	
-    if defined proxy set HTTP_PROXY=%proxy%
-	if defined proxy set HTTPS_PROXY=%proxy%
+	if not exist %rootdir% mkdir %rootdir%
+    ::if not exist %rootdir%\cygwin.exe echo %prefix% ERROR: %rootdir%\cygwin.exe cannot be found. && exit /b 1
+	
+    if "%proxy%"=="" (
+		set http_proxy%proxy%
+		set HTTP_PROXY=%proxy%
+		set HTTPS_PROXY=%proxy%
+	) else (
+		set http_proxy=
+		set HTTP_PROXY=
+		set HTTPS_PROXY=
+	)
     
-	if "%auto_update% == "1" echo %prefix% INFO: Downloading new setup..
-	if "%auto_update% == "1" if /I "%exe%"=="curl.exe" %bindir%\%exe% -so %rootdir%\cygwin.exe http://cygwin.com/setup-%arch%.exe
-	if "%auto_update% == "1" if /I "%exe%"=="wget.exe" %bindir%\%exe% -qO %rootdir%\cygwin.exe http://cygwin.com/setup-%arch%.exe%
+	echo %prefix% Retrieving new setup executable..
+	if /I "%exe%"=="curl.exe" %bindir%\%exe% -so %rootdir%\cygwin.exe http://cygwin.com/setup-%arch%.exe
+	if /I "%exe%"=="wget.exe" %bindir%\%exe% -qO %rootdir%\cygwin.exe http://cygwin.com/setup-%arch%.exe%
 exit /b 0
 
 :HANDLE_ERROR
@@ -145,10 +152,10 @@ exit /b 0
 	:: -o --delete-orphans               remove orphaned packages
 	:: -A --disable-buggy-antivirus      Disable known or suspected buggy anti virus software packages during execution.
 	::
-	if "%proxy%" == ""  %rootdir%\cygwin.exe --quiet-mode --no-shortcuts --upgrade-also --arch %arch% --site %site% --root %rootdir% --local-package-dir %localdir% --packages %packages%
+	if "%proxy%"=="" %rootdir%\cygwin.exe --quiet-mode --no-shortcuts --upgrade-also --arch %arch% --site %site% --root %rootdir% --local-package-dir %localdir% --packages %packages%
 	if "%proxy%" neq "" %rootdir%\cygwin.exe --quiet-mode --no-shortcuts --upgrade-also --arch %arch% --site %site% --root %rootdir% --local-package-dir %localdir% --packages %packages% --proxy %proxy%
-	if "%installed%" == "0" %rootdir%\bin\sed.exe -i -r -e 's/^set installed=0$/set installed=1/' %kncroot%/config.bat
-	%rootdir%\bin\sed.exe -i -r -e 's/^set updated=.*$/set updated=%date%/' %kncroot%/config.bat
+	if "%installed%"=="0" %rootdir%\bin\sed.exe -i -r -e 's/^set installed=0$/set installed=1/' %root%/config.bat
+	%rootdir%\bin\sed.exe -i -r -e 's/^set updated=.*$/set updated=%date%/' %root%/config.bat
 exit /b 0
 
 :PERMISSIONS
@@ -224,17 +231,17 @@ exit /b 0
         getadmin.vbs && del getadmin.vbs
         exit /b 0
     :GOT_ADMIN
+	cmd /k "cd /d %~dp0"
 exit /b 0
 
 :: -- MAIN --
 :MAIN
 	cls
-	call :GET_ADMIN
-	pause
 	call :INIT || goto :HANDLE_ERROR
 	for %%a in (%*) do (if "%%a"=="c" call :CONFIG)
 	for %%a in (%*) do (if "%%a"=="u" call :INSTALL_UPDATE || goto :HANDLE_ERROR)
     for %%a in (%*) do (if "%%a"=="r" call :REMOVE || goto :HANDLE_ERROR)
     for %%a in (%*) do (if "%%a"=="p" call :PERMISSIONS || goto :HANDLE_ERROR)
+	for %%a in (%*) do (if "%%a"=="a" goto :GET_ADMIN)
 	if "%1"=="" call :RUN || goto :HANDLE_ERROR
 exit /b 0
