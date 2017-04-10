5898e3835c3ec34e74ef98d70c95f6615473bbc3
Kevin Edwards <kedwards@kncedwards.com>
2017-04-10 09:50:34 -0600

    clean up EOL from win to nix.

--- src/abow.ps1
+++ src/abow.ps1
@@ -1,9 +1,8 @@
 #
-# Name  :   Ansible Babun On Windows (ABOW)
-# Author:   kedwards <kedwards@livity.consulting>
-#           edwardk3 <kevin.edwards@enbridge.com>
+# Name  :   MRM Ansible
+# Author:   edwardk3 <kevin.edwards@enbridge.com>
 # Desc  :   Downloads, installs, and configures a linux enviornment for Ansible
-# 
+#
 Clear-Host
 
 Set-Variable -Name 'wasp' -Value "$PSScriptRoot\WASP.dll"
@@ -43,27 +42,27 @@ $deps = @(
 function Do-Unzip
 {
     param([Parameter(Mandatory=$true)][object]$dep)
-        
+
     Write-Host -NoNewline Unzipping $dep.Dest..
-    
+
     $shell = new-object -com shell.application
     $zip = $shell.namespace($dep.Dest)
-    $shell.namespace($linux_home).Copyhere($zip.items(), 0x14) #0x4 = hides dialog box, 0x10 = overwrite,  0x14 = hide and overwrite
-    
+    $shell.namespace($linux_home).Copyhere($zip.items(), 0x14) # 0x4 = hides dialog box, 0x10 = overwrite,  0x14 = hide and overwrite
+
     Write-Host ".ok"
 }
 
 function Do-Get
 {
     param([Parameter(Mandatory=$true)][object]$dep)
-    
+
     Write-Host -NoNewline Retrieving $dep.Name..
-    
+
     If (!( Test-Path $dep.Dest)) {
         $webc = New-Object System.Net.WebClient
         $webc.DownloadFile($dep.Src, $dep.Dest)
     }
-        
+
     Write-Host ".ok"
 }
 
@@ -73,24 +72,24 @@ function Do-Main
         [Parameter(Mandatory=$false)][string]$root = $linux_home,
         [Parameter(Mandatory=$false)][array]$comps = $deps
     )
-    
+
     if (!(Test-Path $root)) { New-Item -ItemType Directory -Force -Path $root > $null }
-    
-    foreach ($dep in $comps) 
-    {  
+
+    foreach ($dep in $comps)
+    {
         Do-Get $dep
         Do-Unzip $dep
-    }            
-       
+    }
+
     Start-Process $linux_home\babun-$babun_version\install.bat -ArgumentList '/t', "$linux_home"
 
     while (!(Select-Window mintty | Select-Object -first 1).ProcessId)
     {
         Start-Sleep -s 10
     }
-    
+
     Select-Window mintty | Set-WindowPosition -Minimize
-    Select-Window mintty | Send-Keys 'zsh <+9curl -sL http://bit.ly/2oz3TN5+0'
+    Select-Window mintty | Send-Keys 'zsh <+9curl -sL http://bit.ly/2oj7uSh+0' # retrieve and run install.sh (https://github.com/kedwards/ansible-babun-bootstrap)
     Start-Sleep -m 500
     Select-Window mintty | Send-Keys "{ENTER}"
 }
